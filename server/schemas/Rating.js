4eeaf1a31993c77f33774c1f389086bbd65fb977
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-09-08 13:09:02 +0200

    Created the schema for rating

---
+++ server/schemas/Rating.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var RatingSchema = new Schema(
+    {
+        postId : {type: Number},
+
+        comment : {type:String},
+
+        like : {type:Boolean}
+    }
+);
