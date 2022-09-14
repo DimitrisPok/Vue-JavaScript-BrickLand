3390ee31b2f86e6913ec93f0488aa796242688c5
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-09-14 23:40:09 +0200

    user controller methods are finished

---
+++ server/schemas/Review.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ReviewSchema = new Schema(
+    {
+        postId : {
+            type: Number
+        },
+
+        comment : {
+            type:String
+        },
+
+        like : {
+            type:Boolean,
+            default: false
+        }
+    }
+);
