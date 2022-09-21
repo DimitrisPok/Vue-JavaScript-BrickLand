fb055573f043dd18dd7235dc885f67231c710496
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-09-21 13:40:47 +0200

    testing part 1

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -20,15 +20,8 @@ var storageMulti = multer.diskStorage({
     } 
 }
 );
-var upload = multer({
-    storage:storageMulti});
+var upload = multer({storage:storageMulti});
 
-
-
-
-
-
-    
 router.post("/single", upload.single('img'), async (req,res)=>
 {
     var post = new Post(req.body);
