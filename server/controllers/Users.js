96534ccdfd0598aa2d1de83315484c46a1eac3b6
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-09-21 14:18:54 +0200

    Changes in line 190 from sned to json

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -187,7 +187,7 @@ router.get("/users/:id", function (req, res, next) {
     .populate("reviews")
     .exec(function (err, user) {
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).json(err);
       }
       return res.status(200).send(user);
     });
