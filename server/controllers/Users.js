e415a95dd1ffef05c4dbd5bfd40b1d5342c334ad
SadhanaAnandan <102043923+SadhanaAnandan@users.noreply.github.com>
2022-10-17 22:47:41 +0200

    Fixed again

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -180,7 +180,7 @@ router.get("/users/:user_id/posts/:post_id", function (req, res, next) {
       if (err) {
         return res.status(500).send(err);
       }
-      if (post=null){
+      if (posts=null){
         return res.status(404).json({"message":"Post not found"});
       }
       return res.status(200).json(user.posts);
