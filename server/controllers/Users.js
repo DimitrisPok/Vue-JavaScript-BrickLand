bfc7d58d05f1fcbe8b4f20297247e780b7dfa306
eliassam <gussamanel@student.gu.se>
2022-10-17 15:22:44 +0200

    test

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -179,7 +179,7 @@ router.get("/users/:user_id/posts/:post_id", function (req, res, next) {
       if (err) {
         return res.status(500).send(err);
       }
-      return res.status(200).json(user.posts);
+      return res.status(200).json(user.post);
     });
 }); 
 
